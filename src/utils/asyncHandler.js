11bee3576939f7cf1291b8cc3af8c097793322cb
Mr. kuldeep <kkharoliya20@gmail.com>
2024-08-11 12:36:50 +0530

    error and response to api

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,17 +1,23 @@
-const asyncHandler = (funcToHandle) => {
-async (req,res,next)=>{
-    try {
-      await funcToHandle(req,res,next) 
-    } catch (error) {
-        console.log("error");
-        res.status(501).json({
-            message:"kuch to gdbd he dya",
-            error:error
-        })
-    }
-}
+// const asyncHandler = (funcToHandle) => {
+// async (req,res,next)=>{
+//     try {
+//       await funcToHandle(req,res,next)
+//     } catch (error) {
+//         console.log("error");
+//         res.status(error.code || 501).json({
+//             message:"kuch to gdbd he dya",
+//             seccess:false,
+//             error:error
+//         })
+//     }
+// }
 
+// }
 
-}
+const asyncHandler = (funToHandle) => {
+  (req, res, next) => {
+    Promise.resolve(funToHandle(req, res, next)).catch((error) => next(error));
+  };
+};
 
-export default asyncHandler
+export default asyncHandler;
